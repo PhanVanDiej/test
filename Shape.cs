9b6e246d2983f3d85bcad5ae16b31b64c3d58d32
PhanVanDiej <22520182@gm.uit.edu.vn>
2023-10-04 09:35:36 +0700

    test reposirity

---
+++ Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace myproject
+{
+    abstract class Shape
+    {
+            public abstract double Acreage();
+            public abstract void Nhap();
+            public abstract void Draw();
+            public abstract void Xuat();
+    }
+    class Circle : Shape
+    {
+        int radius; // bán kính
+        Circle(int a,int b)
+        {
+            radius=a;
+            G=b;
+        }
+        public override double Acreage()
+        {
+            return (double) radius*radius*3.14;
+        }
+        public override void Draw()
+        {
+            Console.WriteLine("Hình tròn (G,")
+        }
+    }
+    class Program
+    {
+        public static void main()
+        {
+            int n;
+            Console.WriteLine("Nhap so luong hinh : ");
+            n=Console.Read();
+
+        }
+    }
+}
